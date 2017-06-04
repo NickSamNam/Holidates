84f8f2cf655dc074f55e61a959688cfbde6d38cf
S. Nicknam <s.nicknam@icoud.com>
2017-06-04 20:36:43 +0200

    Initial commit

---
+++ app/src/main/java/com/nicknam/holidates/HolidayItem.java
@@ -0,0 +1,13 @@
+package com.nicknam.holidates;
+
+import java.util.List;
+
+/**
+ * Created by snick on 4-6-2017.
+ */
+
+public class HolidayItem {
+    private String name;
+    private boolean compulsory;
+    private List<Period> periods;
+}
---
+++ app/src/main/java/com/nicknam/holidates/Period.java
@@ -0,0 +1,14 @@
+package com.nicknam.holidates;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by snick on 4-6-2017.
+ */
+
+public class Period implements Serializable {
+    private String region;
+    private Date startDate;
+    private Date endDate;
+}
